0d3e8667e758e6f3c17d240b0c7e3b18de72554b
JBreals <wjdqlsrla0309@naver.com>
2025-06-05 09:59:55 +0900

    최신화

--- api_server-network/inference-server/src/main.py
+++ api_server-network/inference-server/src/main.py
@@ -37,7 +37,7 @@ def run_inference(request: ModelUploadRequest):
     exp_name = request.exp_name
     run_id = request.run_id
     pkl_file = request.pkl_file
-    
+
     BUCKET_NAME = "mlops-weather"
     MODEL_S3_KEY = f"data/deploy_volume/model/{exp_name}/{run_id}/artifacts/model/artifacts/{pkl_file}"
     LOCAL_MODEL_PATH = os.path.join("model", pkl_file)
@@ -50,7 +50,7 @@ def run_inference(request: ModelUploadRequest):
 
         # 2. 추론
         model = joblib.load(LOCAL_MODEL_PATH)
-        future = pd.date_range(start=pd.Timestamp.now(), periods=24, freq="H")
+        future = pd.date_range(start=pd.Timestamp.now(), periods=168, freq="H")
         df_future = pd.DataFrame({"ds": future})
         forecast = model.predict(df_future)
         result = forecast[["ds", "yhat"]].copy()
